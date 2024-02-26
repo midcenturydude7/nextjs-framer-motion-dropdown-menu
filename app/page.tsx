f07150a7bdefb41d599d678bb1c3785fb159a7ec
midCenturyDude7 <mgriffes@gmail.com>
2024-02-25 21:15:04 -0800

    Setting up Menu with framer-motion animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import App from "./components/AppComponent/App";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-800 text-slate-300">
-      Main Page
+      <App />
     </div>
   );
 }
